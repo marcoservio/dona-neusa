eb7a12ef63037e591b18f2fd684c93592946219b
Marco Sérvio <marcoservio22@hotmail.com>
2022-11-21 09:42:01 -0300

    Modificações no designer do relatorio. Foi adicionado o modelo de Cliente e Email. Foi adicionado um campo no formulario de venda para entrar com o nome do cliente. Foi criado uma variavel no relatorio para receber o nome do cliente. Foi implementado a tecla de atalho CRTL + p para imprimir. Foram corrigidos alguns bugs.

---
+++ PastelECia/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿namespace PastelECia.Models
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+            Id = 0;
+            Nome = string.Empty;
+            CnpjCpf = string.Empty;
+            Telefone = string.Empty;
+            Email = string.Empty;
+            Endereco = new Endereco();
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CnpjCpf { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public Endereco Endereco { get; set; }
+    }
+}
---
+++ PastelECia/Models/Endereco.cs
@@ -0,0 +1,28 @@
+﻿namespace PastelECia.Models
+{
+    public class Endereco
+    {
+        public Endereco()
+        {
+            Id = 0;
+            Locradouro = string.Empty;
+            Numero = string.Empty;
+            Complemento = string.Empty;
+            Bairro = string.Empty;
+            Cidade = string.Empty;
+            Pais = string.Empty;
+            Estado = string.Empty;
+            Cep = string.Empty;
+        }
+
+        public int Id { get; set; }
+        public string Locradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Pais { get; set; }
+        public string Estado { get; set; }
+        public string Cep { get; set; }
+    }
+}
